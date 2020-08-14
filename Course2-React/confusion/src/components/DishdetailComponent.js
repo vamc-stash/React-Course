7942fc5699cb22d141b68df2bb5edf709d39dbd9
vamc-stash <rangavamsi5@gmail.com>
2020-08-14 17:17:43 +0530

    fetch POST comment

--- Course2-React/confusion/src/components/DishdetailComponent.js
+++ Course2-React/confusion/src/components/DishdetailComponent.js
@@ -24,7 +24,7 @@ class CommentForm extends Component {
 	}
 
 	handleSubmit(values) {
-		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+		this.props.postComment(this.props.dishId, values.rating, values.author, values.comment)
 		/*console.log("Current state is: "+JSON.stringify(values))
     alert("Current state is: "+JSON.stringify(values))*/
   }
@@ -111,7 +111,7 @@ function FormatDate({dateString}) {
 		return new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(dateString)));
 	}
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
 
 		return(
 				<div className="col-12 col-md-5 m-1">
@@ -126,7 +126,7 @@ function RenderComments({comments, addComment, dishId}) {
 								)
 						})}
 					</ul>
-					<CommentForm dishId={dishId} addComment={addComment}/>
+					<CommentForm dishId={dishId} postComment={postComment}/>
 			</div>
 		)
 }
@@ -168,7 +168,7 @@ const DishDetail = (props) => {
 					<div className="row">
 					 <RenderDish dish = {props.dish}/>
 					 <RenderComments comments = {props.comments}
-					 addComment={props.addComment} dishId={props.dish.id}/>
+					 postComment={props.postComment} dishId={props.dish.id}/>
 					</div>
 				)}
 			</div>
