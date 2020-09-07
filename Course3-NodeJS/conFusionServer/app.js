fe7b5ad7f26bfcf70ffc505a5254f81e57a2a500
vamc-stash <rangavamsi5@gmail.com>
2020-09-07 19:55:13 +0530

    conFusion exercise-Express REST Mongoose Part-1

--- Course3-NodeJS/conFusionServer/app.js
+++ Course3-NodeJS/conFusionServer/app.js
@@ -11,6 +11,17 @@ const dishRouter = require('./routes/dishRouter')
 const promoRouter = require('./routes/promoRouter')
 const leaderRouter = require('./routes/leaderRouter')
 
+const mongoose = require('mongoose')
+
+const Dishes = require('./models/dishes')
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url)
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 var app = express();
 
 // view engine setup
