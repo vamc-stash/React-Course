09c0ae06096a98a6469b3eb5ad53d6d41cf19161
vamc-stash <rangavamsi5@gmail.com>
2020-09-08 18:42:44 +0530

    Express Session part-2

--- Course3-NodeJS/conFusionServer/app.js
+++ Course3-NodeJS/conFusionServer/app.js
@@ -40,47 +40,25 @@ app.use(session({
 	store: new FileStore()
 }))
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
-
-	//if(!req.signedCookies.user) {
-
 	console.log(req.session)
+
 	if(!req.session.user) {
-		//console.log(req.headers)
-		var authHeader = req.headers.authorization
-		if(!authHeader) {
-			var err = new Error('You are not authenticated!')
-			res.setHeader('WWW-Authenticate', 'Basic')
-			err.status = 401
-			next(err)
-			return
-		}
-
-		var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-		var user = auth[0]
-		var pass = auth[1]
-
-		if (user == 'admin' && pass == 'password') {
-			//res.cookie('user', 'admin', {signed: true})
-			req.session.user = 'admin'
-	  next(); // authorized
-	 } else {
-	  var err = new Error('You are not authenticated!');
-	  res.setHeader('WWW-Authenticate', 'Basic');      
-	  err.status = 401;
-	  next(err);
-	 }
+		var err = new Error('You are not authenticated!')
+		err.status = 403
+		return next(err)
 	}
 	else {
-		//if(req.signedCookies.user === 'admin') {
-		if(req.session.user === 'admin') {
-			console.log('req.session: ', req.session)
+		if(req.session.user === 'authenticated') {
 			next()
 		}
 		else {
 			var err = new Error('You are not authenticated!')
 			err.status = 401
-			next(err)
+			return next(err)
 		}
 	}
 }
@@ -88,9 +66,6 @@ function auth(req, res, next) {
 app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
