6c89a75bc71dafc0616c4d3dade54c497d407343
vamc-stash <rangavamsi5@gmail.com>
2020-09-08 16:04:22 +0530

    Cookies

--- Course3-NodeJS/conFusionServer/app.js
+++ Course3-NodeJS/conFusionServer/app.js
@@ -29,31 +29,45 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('your-secret-key-1234-whatever'));
 
 function auth(req, res, next) {
-	console.log(req.headers)
-	var authHeader = req.headers.authorization
-	if(!authHeader) {
-		var err = new Error('You are not authenticated!')
-		res.setHeader('WWW-Authenticate', 'Basic')
-		err.status = 401
-		next(err)
-		return
+
+	if(!req.signedCookies.user) {
+		console.log(req.headers)
+		var authHeader = req.headers.authorization
+		if(!authHeader) {
+			var err = new Error('You are not authenticated!')
+			res.setHeader('WWW-Authenticate', 'Basic')
+			err.status = 401
+			next(err)
+			return
+		}
+
+		var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
+		var user = auth[0]
+		var pass = auth[1]
+
+		if (user == 'admin' && pass == 'password') {
+			res.cookie('user', 'admin', {signed: true})
+	  next(); // authorized
+	 } else {
+	  var err = new Error('You are not authenticated!');
+	  res.setHeader('WWW-Authenticate', 'Basic');      
+	  err.status = 401;
+	  next(err);
+	 }
 	}
-
-	var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
-	var user = auth[0]
-	var pass = auth[1]
-
-	if (user == 'admin' && pass == 'password') {
-  next(); // authorized
- } else {
-  var err = new Error('You are not authenticated!');
-  res.setHeader('WWW-Authenticate', 'Basic');      
-  err.status = 401;
-  next(err);
- }
+	else {
+		if(req.signedCookies.user === 'admin') {
+			next()
+		}
+		else {
+			var err = new Error('You are not authenticated!')
+			err.status = 401
+			next(err)
+		}
+	}
 }
 
 app.use(auth)
