0999df54727160c2bdd87b6e47cce27146761a0a
vamc-stash <rangavamsi5@gmail.com>
2020-09-08 19:49:23 +0530

    User Authentication with Passport

---
+++ Course3-NodeJS/conFusionServer/authenticate.js
@@ -0,0 +1,8 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+var User = require('./models/user')
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
