1ddf6435fee93352e82fe728b7541eda33efcae4
vamc-stash <rangavamsi5@gmail.com>
2020-09-10 13:43:09 +0530

    Passport Facebook OAuth2.0

--- Course3-NodeJS/conFusionServer/config.js
+++ Course3-NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'secretKey': 'your-secret-key-1234-whatever',
-	'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+	'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+	'facebook': {
+		clientId: '412168406614010',
+		clientSecret: 'ec4a870756941e9a9d2d6b00c47cf3ce'
+	}
 }
