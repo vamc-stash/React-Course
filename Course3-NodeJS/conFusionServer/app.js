b43d7fda2b2d31449f74266c69b934dc3fc4a78f
vamc-stash <rangavamsi5@gmail.com>
2020-09-10 10:52:50 +0530

    File Upload

--- Course3-NodeJS/conFusionServer/app.js
+++ Course3-NodeJS/conFusionServer/app.js
@@ -17,6 +17,7 @@ var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter')
 const promoRouter = require('./routes/promoRouter')
 const leaderRouter = require('./routes/leaderRouter')
+const uploadRouter = require('./routes/uploadRouter')
 
 const mongoose = require('mongoose')
 
@@ -28,6 +29,16 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+
+//accepting secure traffic only
+app.all('*', (req, res, next) => {
+	if(req.secure) {
+		return next()
+	}
+	else {
+		res.redirect(307, 'https://' + req.hostname + ":" + app.get('secPort') + req.url)
+	}
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -71,6 +82,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
+app.use('/imageUpload', uploadRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
